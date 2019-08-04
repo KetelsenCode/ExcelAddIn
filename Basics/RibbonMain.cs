27099054baa0fbd65e82c24736ed0f54abad0a23
KetelsenCode <pardonmychessmate@gmail.com>
2019-08-04 12:39:22 +0200

    Add project files.

---
+++ Basics/RibbonMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace Basics
+{
+    public partial class RibbonMain
+    {
+        private void RibbonMain_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
