001c629633992b1bd4b0ac5f11f84bd94a5ae8af
KetelsenCode <pardonmychessmate@gmail.com>
2019-08-04 13:07:34 +0200

    Basics done

--- Basics/RibbonMain.Designer.cs
+++ Basics/RibbonMain.Designer.cs
@@ -34,23 +34,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tab1 = Factory.CreateRibbonTab();
-            this.group1 = Factory.CreateRibbonGroup();
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.btn1 = this.Factory.CreateRibbonButton();
+            this.btn2 = this.Factory.CreateRibbonButton();
+            this.lb1 = this.Factory.CreateRibbonLabel();
+            this.eb1 = this.Factory.CreateRibbonEditBox();
             this.tab1.SuspendLayout();
             this.group1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tab1
             // 
-            this.tab1.Name = "tab1";
             this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
-            this.tab1.ControlId.OfficeId = "TabAddIns";
             this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
             // 
             // group1
             // 
+            this.group1.Items.Add(this.btn1);
+            this.group1.Items.Add(this.btn2);
+            this.group1.Items.Add(this.lb1);
+            this.group1.Items.Add(this.eb1);
             this.group1.Label = "group1";
             this.group1.Name = "group1";
+            // 
+            // btn1
+            // 
+            this.btn1.Label = "insert";
+            this.btn1.Name = "btn1";
+            this.btn1.Click += Btn1_Click;
+            // 
+            // btn2
+            // 
+            this.btn2.Label = "read";
+            this.btn2.Name = "btn2";
+            this.btn2.Click += Btn2_Click;
+            // 
+            // lb1
+            // 
+            this.lb1.Label = "";
+            this.lb1.Name = "lb1";
+            // 
+            // eb1
+            // 
+            this.eb1.Label = "";
+            this.eb1.Name = "eb1";
+            this.eb1.Text = null;
             // 
             // RibbonMain
             // 
@@ -66,10 +97,21 @@
 
         }
 
+
+
+
+
         #endregion
 
         internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
         internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btn1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btn2;
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel lb1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox eb1;
+
     }
 
     partial class ThisRibbonCollection
--- Basics/RibbonMain.cs
+++ Basics/RibbonMain.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Office.Tools.Excel;
 using Microsoft.Office.Tools.Ribbon;
 
 namespace Basics
@@ -11,6 +12,19 @@ namespace Basics
         private void RibbonMain_Load(object sender, RibbonUIEventArgs e)
         {
 
+        }
+
+        private void Btn1_Click(object sender, Microsoft.Office.Tools.Ribbon.RibbonControlEventArgs e)
+        {
+            //Gets the first worksheet in the active workbook
+            Worksheet worksheet = (Worksheet)Globals.Factory.GetVstoObject(Globals.ThisAddIn.Application.ActiveWorkbook.Worksheets[1]);
+            worksheet.Cells[1, 1] = eb1.Text;
+        }
+        private void Btn2_Click(object sender, Microsoft.Office.Tools.Ribbon.RibbonControlEventArgs e)
+        {
+            Worksheet worksheet = (Worksheet)Globals.Factory.GetVstoObject(Globals.ThisAddIn.Application.ActiveWorkbook.Worksheets[1]);
+            var cell = worksheet.Range["A1"];
+            lb1.Label = cell.Text;
         }
     }
 }
